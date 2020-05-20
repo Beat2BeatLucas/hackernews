06fc7ba4fa7789f02afe83d2a409f6995f06582f
Lucas Acuña <lucasnahuelnet@gmail.com>
2020-05-20 17:55:58 -0300

    Split up components

--- src/App.js
+++ src/App.js
@@ -53,14 +53,43 @@ class App extends Component {
     const { list, searchTerm } = this.state;
     return (
       <div className="App">
-        <form>
+        <Search 
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        />
+        <Table 
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
           <input 
             type="text"
-            value={searchTerm}
-            onChange={this.onSearchChange} 
+            value={value}
+            onChange={onChange} 
           />
         </form>
-        { list.filter(isSearched(searchTerm)).map(item => 
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        { list.filter(isSearched(pattern)).map(item => 
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
@@ -70,7 +99,7 @@ class App extends Component {
               <span>{item.points}</span>
               <span>
                 <button
-                  onClick={() => this.onDismiss(item.objectID)}
+                  onClick={() => onDismiss(item.objectID)}
                   type="button"
                 >
                   Dismiss
@@ -82,5 +111,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
