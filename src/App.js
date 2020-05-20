3cb936c0858217a4bb27b020226dc5d6de5480d3
Lucas Acuña <lucasnahuelnet@gmail.com>
2020-05-20 20:16:33 -0300

    Styling Components

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const list = [
   {
     title: 'Redux',
     url: 'https://redux.js.org',
-    author: 'Dan abramov, Andrew Clark',
+    author: 'Dan Abramov, Andrew Clark',
     num_coments: 2,
     points: 5,
     objectID: 1,
@@ -52,13 +52,15 @@ class App extends Component {
   render() {
     const { list, searchTerm } = this.state;
     return (
-      <div className="App">
-        <Search 
-          value={searchTerm}
-          onChange={this.onSearchChange}
-        >
-          Search
-        </Search>
+      <div className="page">
+        <div className="interactions">
+          <Search 
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+            Search
+          </Search>
+        </div>
         <Table 
           list={list}
           pattern={searchTerm}
@@ -73,29 +75,36 @@ export default App;
 
 const Search= ({ value, onChange, children }) =>
   <form>
-    {children}
-      <input 
+    {children} <input 
         type="text"
         value={value}
         onChange={onChange} 
       />
+      
     </form>
 
 
 
 const Table= ({ list, pattern, onDismiss }) => 
-  <div>
+  <div className="table">
     { list.filter(isSearched(pattern)).map(item => 
-        <div key={item.objectID}>
-          <span>
+        <div key={item.objectID} className="table-row">
+          <span style={{ width: '40%'}}>
             <a href={item.url}>{item.title}</a>
           </span>
-          <span>{item.author}</span>
-          <span>{item.num_coments}</span>
-          <span>{item.points}</span>
-          <span>
+          <span style={{ width: '30%'}}>
+            {item.author}
+          </span>
+          <span style={{ width: '10%'}}>
+            {item.num_coments}
+          </span>
+          <span style={{ width: '10%'}}>
+            {item.points}
+          </span>
+          <span style={{ width: '10%'}}>
             <Button
               onClick={() => onDismiss(item.objectID)}
+              className="button-inline"
             >
               Dismiss
             </Button>
